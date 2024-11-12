566cbfc1ba5c2df2467abefb2482087ce8743256
Timothy Thomsson Hydman <146852986+timpip@users.noreply.github.com>
2024-11-12 14:28:39 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
-from main import code
-def response():
-    assert code == 200
+from main import data_SMHI
+
+def test_API_response():
+    df, code = data_SMHI()
+    assert code == 200, f"Expected status code 200, but got {code}"
