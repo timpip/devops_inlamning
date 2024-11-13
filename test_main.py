6f0ea653cef4e35fc4fe87c39aad6d9eff78e928
timpip <timothy.hydman@hotmail.com>
2024-11-13 17:15:23 +0100

    ...

--- test_main.py
+++ test_main.py
@@ -12,4 +12,5 @@ def test_API_response():
 def test_unit():
     samlad_data_dict = data_SMHI()
     
-    assert samlad_data_dict['provider'] == "SMHI"
+    assert samlad_data_dict['provider'].values[0] == "SMHI"
+
