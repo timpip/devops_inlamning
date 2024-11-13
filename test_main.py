f0459eb914d658f070ab73f02332cd0c043fd1fe
timpip <timothy.hydman@hotmail.com>
2024-11-13 17:08:37 +0100

    changing settings in test file

--- test_main.py
+++ test_main.py
@@ -1,11 +1,15 @@
+import requests
 from main import data_SMHI
 
 def test_API_response():
-    df, code, samlad_data_dict = data_SMHI()
+    URL = f"https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0215/lat/59.3099/data.json"
+    response = requests.get(URL)
+    #kolla om ok
+    code = response.status_code
     assert code == 200, f"Expected status code 200, but got {code}"
 
 
 def test_unit():
-    df, code, samlad_data_dict = data_SMHI()
+    samlad_data_dict = data_SMHI()
     
     assert samlad_data_dict['provider'] == "SMHI"
