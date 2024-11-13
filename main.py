7c1f8c07c612952454f90fc5a2c022875579d33a
timpip <timothy.hydman@hotmail.com>
2024-11-13 17:25:52 +0100

    fixes

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import datetime
 import streamlit as st
 import pandas as pd
 
+lon = 18.0215
+lat = 59.3099
+
+
 def data_SMHI():
     now = datetime.datetime.now()
-    lon = 18.0215
-    lat = 59.3099
     formatted_datetime = now.strftime('%Y-%m-%d %H:%M:%S')
     URL = f"https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/{lon}/lat/{lat}/data.json"
     response = requests.get(URL)
